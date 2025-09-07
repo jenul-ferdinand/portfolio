627dda303b66a31ca590dcd6d5b1e0dfde038725
Jenul Ferdinand <jenul15ferdinand@gmail.com>
2025-09-07 19:29:50 +1000

    Add custom Tiempos font setup and Poppins integration

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 export default {
     content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
     theme: {
-        extend: {},
+        extend: {
+            fontFamily: {
+                tiempos: ['Tiempos', 'serif'],
+                poppins: ['Poppins', 'sans-serif']
+            }
+        },
     },
     plugins: [],
 }
